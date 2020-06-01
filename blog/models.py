81bd7e937d5a4833006610d02f541fa82bd3286f
Tonynology <tonycontinue@gamil.com>
2020-05-31 22:46:51 -0700

    blog app - Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30) #제목
+    content = models.TextField()   #내용
+
+    created = models.DateTimeField()  #작성 날,시간
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  #글 저자
+#장고는 admin 페이지를 자동으로 만든다.
