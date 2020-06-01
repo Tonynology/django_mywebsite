f73384d962bda3bbb2c5ddba824274adb9d89b0b
Tonynology <tonycontinue@gamil.com>
2020-06-01 13:26:01 -0700

    Post: list view

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .models import Post
-# Create your views here.
+from django.views.generic import ListView
 
+class PostList(ListView):
+    model = Post
 
-def index(request):
-    posts = Post.objects.all() #장고에서 기본적으로 제공한다.
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all()    #장고에서 기본적으로 제공한다.
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
