609ba469cc99eaa1058a4a27110b88787c41f756
Tonynology <tonycontinue@gamil.com>
2020-06-01 16:10:33 -0700

    show random image when head_image is none

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
     title = models.CharField(max_length=30) #제목
     content = models.TextField()   #내용
 
+    head_image = models.ImageField(upload_to='blog/%Y%m%d/', blank=True)
+
     created = models.DateTimeField()  #작성 날,시간
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)  #글 저자
