f3d7aed8776836d43caa866a2e173965db59a12a
Tonynology <tonycontinue@gamil.com>
2020-05-31 23:25:15 -0700

    blog post list (index)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all() #장고에서 기본적으로 제공한다.
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
