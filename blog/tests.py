8db4351f511bd2101ba103079191aab0204056f8
Tonynology <tonycontinue@gamil.com>
2020-06-03 16:17:25 -0700

    categories in post_detail page

--- blog/tests.py
+++ blog/tests.py
@@ -58,6 +58,11 @@ class TestView(TestCase):
         self.assertIn('Blog', navbar.text)
         self.assertIn('About me', navbar.text)
 
+    def check_right_side(self, soup):
+        category_card = soup.find('div', id='category-card') #### 미분휴 (1) 있어야 함
+        self.assertIn('미분류 (1)', category_card.text)
+        self.assertIn('정치/사회 (1)', category_card.text)  #### 정치 (1) 있어야 함
+
     def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
@@ -102,12 +107,10 @@ class TestView(TestCase):
 
 
         # category card 에서
-        category_card = body.find('div', id='category-card') #### 미분휴 (1) 있어야 함
-        self.assertIn('미분류 (1)', category_card.text)
-        self.assertIn('정치/사회 (1)', category_card.text)  #### 정치 (1) 있어야 함
+        self.check_right_side(soup)
 
         #main_div 에는
-        main_div = body.find('div', id='main_div')
+        main_div = soup.find('div', id='main_div')
         self.assertIn('정치/사회', main_div.text) # 첫번째 포스트에는 '정치/사회' 있어야 함
         self.assertIn('미분류', main_div.text) #### 두번째 ~~~~~~~  '미분류' 있어야함
 
@@ -116,6 +119,13 @@ class TestView(TestCase):
             title='The first post',
             content='Hello World, We are the world',
             author=self.author_000,
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Second Second Second',
+            author=self.author_000,
+            category=create_category(name='정치/사회')
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -137,3 +147,5 @@ class TestView(TestCase):
         self.assertIn(post_000.author.username, main_div.text)
 
         self.assertIn(post_000.content, main_div.text)
+
+        self.check_right_side(soup)
