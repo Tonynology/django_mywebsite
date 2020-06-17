cfbbf40417cd49419d4f153c65b638eed4cd4b86
Tonynology <tonycontinue@gamil.com>
2020-06-17 16:26:10 -0700

    fixed comment bug and prepare working signout page

--- my_site_prj/settings.py
+++ my_site_prj/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'my_site_prj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'allauth_templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
